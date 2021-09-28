88abb728a2b406d50da86a6e2af9be6723fb840c
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-09-28 15:14:05 +0300

    matrix/config/config.cpp

---
+++ matrix/config/config.cpp
@@ -0,0 +1,53 @@
+#include <matrix/config/config.hpp>
+
+namespace whirl::matrix::cfg {
+
+std::string NodeConfig::GetString(std::string_view key) const {
+  // This server
+
+  if (key == "pool") {
+    return server_.pool;
+  }
+
+  // Global constant
+
+  if (key == "db.path") {
+    return "/db";
+  }
+  KeyNotFound(key);
+}
+
+int64_t NodeConfig::GetInt64(std::string_view key) const {
+  // This server
+
+  if (key == "node.id") {
+    return server_.id;
+  }
+
+  // Global constant
+
+  if (key == "rpc.port") {
+    return 42;
+  }
+
+  // Backoff params
+  // Defined by time model
+
+  if (key == "rpc.backoff.init") {
+    return TimeModel()->BackoffParams().init;
+  }
+  if (key == "rpc.backoff.max") {
+    return TimeModel()->BackoffParams().max;
+  }
+  if (key == "rpc.backoff.factor") {
+    return TimeModel()->BackoffParams().factor;
+  }
+
+  KeyNotFound(key);
+}
+
+bool NodeConfig::GetBool(std::string_view key) const {
+  KeyNotFound(key);
+}
+
+}  // namespace whirl::matrix::cfg
--- matrix/config/config.hpp
+++ matrix/config/config.hpp
@@ -15,53 +15,9 @@ class NodeConfig : public node::cfg::IConfig {
   NodeConfig(ServerConfig server_config) : server_(server_config) {
   }
 
-  std::string GetString(std::string_view key) const override {
-    // This server
-
-    if (key == "pool") {
-      return server_.pool;
-    }
-
-    // Global constant
-
-    if (key == "db.path") {
-      return "/db";
-    }
-    KeyNotFound(key);
-  }
-
-  int64_t GetInt64(std::string_view key) const override {
-    // This server
-
-    if (key == "node.id") {
-      return server_.id;
-    }
-
-    // Global constant
-
-    if (key == "rpc.port") {
-      return 42;
-    }
-
-    // Backoff params
-    // Defined by time model
-
-    if (key == "rpc.backoff.init") {
-      return TimeModel()->BackoffParams().init;
-    }
-    if (key == "rpc.backoff.max") {
-      return TimeModel()->BackoffParams().max;
-    }
-    if (key == "rpc.backoff.factor") {
-      return TimeModel()->BackoffParams().factor;
-    }
-
-    KeyNotFound(key);
-  }
-
-  bool GetBool(std::string_view key) const override {
-    KeyNotFound(key);
-  }
+  std::string GetString(std::string_view key) const override;
+  int64_t GetInt64(std::string_view key) const override;
+  bool GetBool(std::string_view key) const override;
 
  private:
   [[noreturn]] static void KeyNotFound(std::string_view key) {
